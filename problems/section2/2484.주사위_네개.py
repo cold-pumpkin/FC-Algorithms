8bb21f317281f022e7539ac1eba1179e1f5608df
adorno <pch4623@gmail.com>
2020-02-14 01:10:00 +0900

    2484. 주사위 네개

---
+++ problems/section2/2484.주사위_네개.py
@@ -0,0 +1,18 @@
+# 2484. 주사위 네개
+
+def calcPrize():
+    arr = sorted(list(map(int, input().split())))
+    if len(set(arr)) == 1:
+        return 50000 + arr[0] * 5000
+    if len(set(arr)) == 2:
+        if arr[1] == arr[2]:
+            return 10000 + arr[1] * 1000
+        else:
+            return 2000 + (arr[0] + arr[2]) * 500
+    for i in range(3):
+        if arr[i] == arr[i+1]:
+            return 1000 * arr[i]
+    return arr[-1] * 100
+
+N = int(input())
+print(max(calcPrize() for _ in range(N)))
